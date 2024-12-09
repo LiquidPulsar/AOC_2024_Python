217812d61640fdda6897c33c0bbf83c3709d6423
Alex <noelalexandre66@gmail.com>
2024-12-09 22:26:49 +0000

    Changed filesystem to list-of-heap approach, 58ms

--- Day_9/p2.py
+++ Day_9/p2.py
@@ -1,47 +1,43 @@
-from collections import deque
 from pathlib import Path
+from heapq import heappop, heappush
 
 HOME = Path(__file__).parent
 
 
-File = tuple[int, int] # id, size
-# stack of free spaces, stack of files
-# list of done (optimise out by calc on the fly)
+File = tuple[int, int, int]  # id, size, ofs
 
-filesys = []
+filesys: list[File] = []
 
 # hack to make length even
-m = map(int,Path(HOME/"input.txt").read_text()+"0")
-for idx,(file,free) in enumerate(zip(m,m)):
-    filesys.extend(((idx, file), free))
-filesys.pop() # remove dummy 0
+m = map(int, Path(HOME / "input.txt").read_text() + "0")
+freemap: dict[int, list[int]] = {k: [] for k in range(1, 10)}
+ofs = 0
+for idx, (file, free) in enumerate(zip(m, m)):
+    filesys.append((idx, file, ofs))
+    ofs += file
+    if free:
+        heappush(freemap[free], ofs)
+        ofs += free
 
-for i in range(len(filesys)-1,-1,-1):
-    if isinstance(filesys[i], int):
-        continue
-    _,length = fi = filesys[i]
-    for j in range(i): # avoid making slices
-        f = filesys[j]
-        if isinstance(f, tuple):
-            continue
-        if f == length:
-            filesys[i] = length
-            filesys[j] = fi
-            break
-        elif f > length:
-            filesys[i] = length
-            filesys[j] -= length
-            filesys.insert(j,fi)
-            break
-
-# print(filesys)
-
-total = ofs = 0
-for d in filesys:
-    if isinstance(d, int):
-        ofs += d
+total = 0
+for id_, length, ofs in reversed(filesys):
+    # index 0 is heapmin
+    loc, space = min(
+        (
+            (frees[0], space)
+            for space, frees in freemap.items()
+            if space >= length and frees
+        ),
+        default=(None, None),
+    )
+    if space is not None:
+        assert loc is not None  # satisfy type checker
+        # print("Found space for", id_, length, space, loc)
+        total += id_ * sum(range(loc, loc + length))
+        heappop(freemap[space])
+        if space > length:
+            heappush(freemap[space - length], loc + length)
     else:
-        idx,length = d
-        total += idx * sum(range(ofs,ofs+length))
-        ofs += length
+        # print("No space for", id_, length, ofs)
+        total += id_ * sum(range(ofs, ofs+length))
 print(total)
