61eefc97833d9e319344985ea08b8359ccc51daa
Alex <noelalexandre66@gmail.com>
2024-12-09 22:54:10 +0000

    41ms hyperfine, changed to tuple

--- Day_9/p2.py
+++ Day_9/p2.py
@@ -4,40 +4,32 @@ from heapq import heappop, heappush
 HOME = Path(__file__).parent
 
 
-File = tuple[int, int, int]  # id, size, ofs
+File = tuple[int, int, int]  # ofs, size, id
 
 filesys: list[File] = []
 
 # hack to make length even
-m = map(int, Path(HOME / "input.txt").read_text() + "0")
-freemap: dict[int, list[int]] = {k: [] for k in range(1, 10)}
+m = map(int, Path(HOME / "test.txt").read_text() + "0")
+freemap: tuple[list[int], ...] = tuple(([] for _ in range(10)))
 ofs = 0
 for idx, (file, free) in enumerate(zip(m, m)):
-    filesys.append((idx, file, ofs))
+    filesys.append((ofs, file, idx))
     ofs += file
     if free:
         heappush(freemap[free], ofs)
         ofs += free
 
 total = 0
-for id_, length, ofs in reversed(filesys):
-    # index 0 is heapmin
-    loc, space = min(
-        (
-            (frees[0], space)
-            for space, frees in freemap.items()
-            if space >= length and frees
-        ),
-        default=(None, None),
-    )
-    if space is not None:
-        assert loc is not None  # satisfy type checker
+for ofs, length, id_ in reversed(filesys):
+    f, space = min(((f, i) for i in range(length, 10) if (f:=freemap[i])), default=(None, 0))
+    if f:
+        loc = heappop(f)
         # print("Found space for", id_, length, space, loc)
         total += id_ * sum(range(loc, loc + length))
-        heappop(freemap[space])
         if space > length:
             heappush(freemap[space - length], loc + length)
     else:
         # print("No space for", id_, length, ofs)
-        total += id_ * sum(range(ofs, ofs+length))
+        total += id_ * sum(range(ofs, ofs + length))
+
 print(total)
