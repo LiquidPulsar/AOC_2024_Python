eb31aaa1fb1c0624c4185f4dc8fe0bc0a65aa4d6
Alex <noelalexandre66@gmail.com>
2024-12-09 23:44:31 +0000

    11.5ms :)

--- Day_9/p2.py
+++ Day_9/p2.py
@@ -4,11 +4,15 @@ from heapq import heappop, heappush
 HOME = Path(__file__).parent
 
 # from time import perf_counter_ns
+
 # tic = perf_counter_ns()
 
-File = tuple[int, int, int]  # ofs, size, id
+# ofs, size, id
+filesys: list[tuple[int, int, int]] = []
 
-filesys: list[File] = []
+
+def range_sum(start: int, end: int) -> int:
+    return start * end + (end * (end - 1)) // 2
 
 # hack to make length even
 m = map(int, Path(HOME / "input.txt").read_text() + "0")
@@ -23,16 +27,19 @@ for idx, (file, free) in enumerate(zip(m, m)):
 
 total = 0
 for ofs, length, id_ in reversed(filesys):
-    f, space = min(((f, i) for i in range(length, 10) if (f:=freemap[i])), default=(None, 0))
+    f, space = min(
+        ((f, i) for i in range(length, 10) if (f := freemap[i]) and f[0] < ofs),
+        default=(None, 0),
+    )
     if f:
         loc = heappop(f)
         # print("Found space for", id_, length, space, loc)
-        total += id_ * sum(range(loc, loc + length))
+        total += id_ * range_sum(loc, length)
         if space > length:
             heappush(freemap[space - length], loc + length)
     else:
         # print("No space for", id_, length, ofs)
-        total += id_ * sum(range(ofs, ofs + length))
+        total += id_ * range_sum(ofs, length)
 
 print(total)
-# print((perf_counter_ns()-tic)/1e6, "ms")
+# print((perf_counter_ns() - tic) / 1e6, "ms")
