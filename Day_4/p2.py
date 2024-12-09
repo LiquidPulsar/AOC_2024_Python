27a649f03c90802c2ce98d3bc82c0c08db9b611d
Alex <noelalexandre66@gmail.com>
2024-12-09 12:21:38 +0000

    Sliding window version of d4p2, why not

--- Day_4/p2.py
+++ Day_4/p2.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from more_itertools import sliding_window
 
 HOME = Path(__file__).parent
 
@@ -7,16 +8,12 @@ valid = ((TARGET[0], TARGET[2]), (TARGET[2], TARGET[0]))
 
 total = 0
 with open(HOME / "input.txt") as f:
-    data = f.read().splitlines()
-
-    X, Y = len(data[0]), len(data)
-
-    for y, row in enumerate(data[: -len(TARGET) + 1]):
-        for x, col in enumerate(row[: -len(TARGET) + 1]):
+    for rs in sliding_window(f, len(TARGET)):
+        for c1,c2,c3 in sliding_window(zip(*rs), len(TARGET)):
             if (
-                data[y + 1][x + 1] == TARGET[1]
-                and (col, data[y + 2][x + 2]) in valid
-                and (data[y][x + 2], data[y + 2][x]) in valid
+                c2[1] == TARGET[1]
+                and (c1[0], c3[2]) in valid
+                and (c1[2], c3[0]) in valid
             ):
                 total += 1
     print(total)
