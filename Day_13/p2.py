63a48b87d64e82efc7929fc22a23f1bf95f39d97
Alex <noelalexandre66@gmail.com>
2024-12-14 13:38:47 +0000

    Day 13 improvement

--- Day_13/p2.py
+++ Day_13/p2.py
@@ -5,27 +5,17 @@ import re
 HOME = Path(__file__).parent
 
 games = re.findall(
-    r"X\+(\d+), Y\+(\d+)\n"
-    r".*X\+(\d+), Y\+(\d+)\n"
-    r".*X=(\d+), Y=(\d+)", 
-    (HOME / "input.txt").read_text()
+    r"X\+(\d+), Y\+(\d+)\n" r".*X\+(\d+), Y\+(\d+)\n" r".*X=(\d+), Y=(\d+)",
+    (HOME / "input.txt").read_text(),
 )
 
 total = 0
 for game in games:
-    x1,y1,x2,y2,tx,ty = map(int,game)
-    t = np.array([10000000000000+tx,10000000000000+ty])
+    x1, y1, x2, y2, tx, ty = map(int, game)
+    t = np.array([10000000000000 + tx, 10000000000000 + ty])
 
-    m = np.array([
-        [x1, x2],
-        [y1, y2]
-    ])
-
-
-    cost = c1,c2 = np.linalg.inv(m) @ t
-    # print(cost)
-    # print(f"{c1:.20f} {c2:.20f}")
-    if np.all(np.dot(m,list(map(round,cost))) == t) and c1 >= 0 and c2 >= 0:
-        # print(3*c1 + c2)
-        total += 3*c1 + c2
+    m = np.array([[x1, x2], [y1, y2]])
+    cost = np.round(np.linalg.inv(m) @ t)
+    if np.all(m @ cost == t) and np.all(cost >= 0):
+        total += cost @ np.array([3, 1])
 print(total)
