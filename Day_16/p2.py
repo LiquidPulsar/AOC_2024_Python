0aca9d530fd4acaafb3aa2f5a872c3c4c41663e9
Alex <noelalexandre66@gmail.com>
2024-12-16 14:05:32 +0000

    Cleaned up Day 16 p2

--- Day_16/p2.py
+++ Day_16/p2.py
@@ -8,25 +8,11 @@ grid = (HOME / "input.txt").read_text().splitlines()
 start = (1, len(grid) - 2)
 end = (len(grid[0]) - 2, 1)
 
-
-def display(grid, path, ex, ey):
-    for y, row in enumerate(grid):
-        for x, c in enumerate(row):
-            if x == ex and y == ey:
-                print("X", end="")
-            elif (x, y) in path:
-                print("*", end="")
-            else:
-                print(c, end="")
-        print()
-    print()
-
-
-heap: list[tuple[int, int, list[tuple[int, int]]]] = [(0, 0, [start])]
+heap: list[tuple[int, int, tuple[int, int]]] = [(0, 0, start)]
 dct = defaultdict(lambda: 1000000000000)
 target_time = None
 while heap:
-    t, d, [*path, (x, y)] = heappop(heap)
+    t, d, (x, y) = heappop(heap)
     # print(t)
     if t >= dct[(x, y, d)]:
         continue
@@ -40,23 +26,25 @@ while heap:
         if target_time is None:
             target_time = t
         print("Done:", t)
-        
+
     for nd, (dx, dy) in enumerate([(1, 0), (0, 1), (-1, 0), (0, -1)]):
         nx, ny = x + dx, y + dy
         if grid[ny][nx] != "#":
             # 0 -> 3
             abs_rot = abs(d - nd)
             rot = min(abs_rot, 4 - abs_rot)
-            heappush(heap, (t + 1 + 1000 * rot, nd, [*path, (x, y), (nx, ny)]))
+            heappush(heap, (t + 1 + 1000 * rot, nd, (nx, ny)))
 
 assert target_time is not None
 
 # Backsolve
-stack: list[tuple[int, int, list[tuple[int, int]]]] = [(target_time, d, [end]) for d in range(4) if (*end, d) in dct]
+stack: list[tuple[int, int, tuple[int, int]]] = [
+    (target_time, d, end) for d in range(4) if (*end, d) in dct
+]
 seen = set()
 
 while stack:
-    t, d, [*path, (x, y)] = stack.pop()
+    t, d, (x, y) = stack.pop()
     # print(t, d, path)
     seen.add((x, y))
 
@@ -70,7 +58,6 @@ while stack:
             # print([dct.get((nx, ny, i)) for i in range(4)])
             # print((nx, ny, nd),dct[(nx, ny, nd)],t - 1 - 1000 * rot)
             if dct[(nx, ny, nd)] == t - 1 - 1000 * rot:
-                stack.append((t - 1 - 1000 * rot, nd, [*path, (x, y), (nx, ny)]))
+                stack.append((t - 1 - 1000 * rot, nd, (nx, ny)))
 
 print(len(seen))
-display(grid, seen, -1, -1)
