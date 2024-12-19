331008f5578c7b24a8deb15ddcef717914abc05d
Alex <noelalexandre66@gmail.com>
2024-12-19 05:26:35 +0000

    Day 19 done

---
+++ Day_19/p2.py
@@ -0,0 +1,21 @@
+from functools import cache
+from pathlib import Path
+
+from tqdm import tqdm
+
+HOME = Path(__file__).parent
+
+with open(HOME/"input.txt") as f:
+    patterns = f.readline().rstrip().split(", ")
+    f.readline()
+    designs = f.read().splitlines()
+
+@cache
+def solve(design):
+    if not design: return True
+    return sum(
+        design.startswith(pattern) and solve(design[len(pattern) :])
+        for pattern in patterns
+    )
+
+print(sum(solve(design) for design in tqdm(designs)))
