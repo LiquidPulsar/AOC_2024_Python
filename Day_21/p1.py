478f8c955aeab6a80b6050e2f82d89e2ffdabed4
Alex <noelalexandre66@gmail.com>
2024-12-21 12:48:10 +0000

    Day 21: Make p1 p2 style

--- Day_21/p1.py
+++ Day_21/p1.py
@@ -1,3 +1,4 @@
+from functools import cache
 from pathlib import Path
 
 HOME = Path(__file__).parent
@@ -10,67 +11,45 @@ HOME = Path(__file__).parent
 +---+---+---+
 | 1 | 2 | 3 |
 +---+---+---+
-    | 0 | A |
-    +---+---+
-"""
-
-keypad = [[7,8,9],[4,5,6],[1,2,3],[None,0,"A"]]
-pos_dct = {str(v):(j,i) for i,row in enumerate(keypad) for j,v in enumerate(row) if v is not None}
-
-codes = (HOME/"input.txt").read_text().splitlines()
-
-# D -> D -> D -> N
-
-"""
-    +---+---+
-    | ^ | A |
+    |0/^| A |
 +---+---+---+
 | < | v | > |
 +---+---+---+
-
-Always starts on A, will be at A before each instr
-
 """
 
-num_keypad = [[None, "^", "A"],[ "<", "v", ">"]]
-num_pos_dct = {v:(j,i) for i,row in enumerate(num_keypad) for j,v in enumerate(row) if v is not None}
+# replace ^ with 0 to combine both keypads
+keypad = "789 456 123 _0A <v>".split()
+pos_dct = {v:(j,i) for i,row in enumerate(keypad) for j,v in enumerate(row) if v != "_"}
+dead_Y, dead_X = 3, 0
+codes = (HOME/"input.txt").read_text().splitlines()
 
-strs = dict.fromkeys(range(4), "")
+@cache
+def paths(a,b):
+    x,y = pos_dct[a]
+    nx,ny = pos_dct[b]
+    dx,dy = abs(nx-x),abs(ny-y)
+    # Solve horizontal then vertical
+    # But we could hit a dead spot if
+    # y on row with dead spot and nx = 0
+    c = "<>"[nx > x]*dx + "0v"[ny > y]*dy
 
-def cost(code: str, pos_dct, level=0) -> int:
+    cs = []
+    if y != dead_Y or nx != dead_X:
+        cs.append(f"{c}A")
+    if dx and dy and (x != dead_X or ny != dead_Y):
+        cs.append(f"{c[::-1]}A")
+    return cs
+
+@cache
+def cost(code: str, level=0) -> int:
+    if level == 0:
+        return len(code)
     total = 0
-    x,y = pos_dct["A"]
-    dead_row = 3 * (level == 3)
-    for c in code:
-        nx,ny = pos_dct[c]
-        dx,dy = nx-x,ny-y
-        if level == 0:
-            total += 1
-        else:
-            c = ""
-            # Solve horizontal then vertical
-            # But we could hit a dead spot if
-            # y on row with dead spot and nx = 0
-            c += "<>"[nx > x]*abs(nx-x)
-            c += "^v"[ny > y]*abs(ny-y)
-
-            cs = []
-            if y != dead_row or nx != 0:
-                cs.append(c)
-            if dx and dy and (x != 0 or ny != dead_row):
-                cs.append(c[::-1])
-            costs = [cost(f"{c}A", num_pos_dct, level-1) for c in cs]
-            # if any(cost != costs[0] for cost in costs):
-            #     print(cs, costs, level)
-            total += min(costs)
-        x,y = nx,ny
+    curr = "A"
+    for nxt in code:
+        total += min(cost(path, level-1) for path in paths(curr,nxt))
+        curr = nxt
     return total
 
-total = 0
-for code in codes:
-    cst = cost(code, pos_dct, 3)
-    total += cst * int(code[:-1])
-    # print(code, cost(code, pos_dct, 3))
-    # strs[3] = code
-    # print(strs)
+total = sum(cost(code, 3) * int(code[:-1]) for code in codes)
 print(total)
