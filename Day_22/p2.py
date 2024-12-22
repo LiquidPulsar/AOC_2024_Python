b759103cae6ce3a2d329972e275d63b2865c43cc
Alex <noelalexandre66@gmail.com>
2024-12-22 17:04:36 +0000

    Day 22: Multiprocessing: down to 1.35s in critical section

--- Day_22/p2.py
+++ Day_22/p2.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+from multiprocessing import Pool
 from pathlib import Path
 
 from time import perf_counter
@@ -10,11 +11,10 @@ HOME = Path(__file__).parent
 with open(HOME / "input.txt") as f:
     nums = [*map(int, f)]
 
-total_dct = defaultdict(int)
-for num in nums:
+def worker(num: int) -> dict[tuple[int, int, int, int], int]:
     window = deque(maxlen=4)
     prev_price = num
-    seen = set()
+    dct = {}
     for _ in range(2000):
         num ^= num << 6
         num &= PRUNE
@@ -25,15 +25,22 @@ for num in nums:
         price = num % 10
         window.append(price - prev_price)
         key = tuple(window)
-        if key not in seen:
-            total_dct[key] += price
-            seen.add(key)
+        if key not in dct:
+            dct[key] = price
         prev_price = price
+    return dct
 
-t1 = perf_counter()
-print(t1 - t0)
+if __name__ == "__main__":
+    total_dct = defaultdict(int)
+    with Pool() as pool:
+        for result in pool.imap_unordered(worker, nums):
+            for k, v in result.items():
+                total_dct[k] += v
 
-print(*max(total_dct.items(), key=lambda x: x[1]))
+    t1 = perf_counter()
+    print(t1 - t0)
 
-t2 = perf_counter()
-print(t2 - t1)
+    print(*max(total_dct.items(), key=lambda x: x[1]))
+
+    t2 = perf_counter()
+    print(t2 - t1)
