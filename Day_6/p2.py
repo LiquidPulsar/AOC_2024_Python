3086cd8480a786627bbb632e18a0c16061d08165
Alex <noelalexandre66@gmail.com>
2024-12-27 02:58:21 +0000

    Multiprocess Day 6

--- Day_6/p2.py
+++ Day_6/p2.py
@@ -1,32 +1,14 @@
 from collections import defaultdict
 from pathlib import Path
 
-from tqdm import tqdm
-
+from multiprocessing import Pool
 
 Pos = tuple[int, int]
 HOME = Path(__file__).parent
 
-start = None
-with open(HOME / "input.txt") as f:
-    grid = f.read().splitlines()
-    H, W = len(grid), len(grid[0])
-    for i, row in enumerate(grid):
-        for j, col in enumerate(row):
-            if col == "^":
-                start = (i, j)
-                break
-        else:
-            continue
-        break
-
-assert start is not None, "No start position found"
-
 deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-d = 0
 
-
-def loops(curr, grid):
+def loops_start(curr, grid, H, W):
     d = 0
     cache: dict[Pos, list[int]] = defaultdict(list)
 
@@ -43,18 +25,40 @@ def loops(curr, grid):
             curr = new
     return True, cache
 
-start_loops, cache = loops(start, grid)
-assert not start_loops, "Start position loops"
+def loops(args):
+    curr, obstacle, grid, H, W = args
+    d = 0
+    cache: dict[Pos, list[int]] = defaultdict(list)
 
-total = 0
-grid = [*map(list, grid)]
-for i,row in tqdm(enumerate(grid), total=H):
-    for j, col in enumerate(row):
-        if (i,j) not in cache:
-            continue
-        row[j] = "#"
-        if loops(start, grid)[0]:
-            total += 1
-        row[j] = "."
+    while d not in cache[curr]:
+        cache[curr].append(d)
+        i, j = curr
+        di, dj = deltas[d]
+        new = ni, nj = i + di, j + dj
+        if not 0 <= ni < H or not 0 <= nj < W:
+            return False
+        if grid[ni][nj] == "#" or new == obstacle:
+            d = (d + 1) % 4
+        else:
+            curr = new
+    return True
 
-print(total)
+def main():
+    start = None
+    with open(HOME / "input.txt") as f:
+        grid = f.read().splitlines()
+        H, W = len(grid), len(grid[0])
+        start = next((i, j) for i, row in enumerate(grid) for j, col in enumerate(row) if col == "^")
+
+    assert start is not None, "No start position found"
+
+    start_loops, cache = loops_start(start, grid, H, W)
+    assert not start_loops, "Start position loops"
+
+    grid = [*map(list, grid)]
+    start_opts = ((start, (i,j), grid, H, W) for (i,j) in cache)
+    with Pool() as pool:
+        print(sum(pool.imap_unordered(loops, start_opts, chunksize=25))) # Found by hyperfine param scan
+
+if __name__ == "__main__":
+    main()
